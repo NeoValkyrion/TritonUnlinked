7cc9cf5b13011a88fa58b632fe3c98e2dd5d0f70
NeoValkyrion <dan@bestangle.com>
2014-02-22 14:31:34 -0800

    WIP for Android database stuff

---
+++ TritonUnlinked/src/main/java/triton/unlinked/CourseModel.java
@@ -0,0 +1,41 @@
+package triton.unlinked;
+
+/**
+ * Created by NeoValkryion on 2/19/14.
+ */
+public class CourseModel {
+
+    private long id;
+    private String subject;
+    private String number;
+    private String desc;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject() {
+        this.subject = subject;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber() {
+        this.number = number;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc() {
+        this.desc = desc;
+    }
+
+}
