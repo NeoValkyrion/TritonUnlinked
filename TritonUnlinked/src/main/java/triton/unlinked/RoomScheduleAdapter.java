1330dfd0c60cbf9344d9287e550617287245cfa2
Kimberly Chu <k4chu@ucsd.edu>
2014-03-02 16:24:01 -0800

    added a second box to the listview item for the room schedule because it might be needed to display the classes on the schedule (ex: class is from 8:30-9:50)

--- TritonUnlinked/src/main/java/triton/unlinked/RoomScheduleAdapter.java
+++ TritonUnlinked/src/main/java/triton/unlinked/RoomScheduleAdapter.java
@@ -5,8 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.util.TypedValue;
 
 public class RoomScheduleAdapter extends ArrayAdapter<String> {
     private final Context context;
@@ -23,9 +24,26 @@ public class RoomScheduleAdapter extends ArrayAdapter<String> {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.room_schedule_row, parent, false);
-        TextView textView = (TextView) rowView.findViewById(R.id.label);
-        //ImageView imageView = (ImageView) rowView.findViewById(R.id.line);
-        textView.setText(values[position]);
+        TextView time = (TextView) rowView.findViewById(R.id.time);
+        time.setText(values[position]);
+
+        //TODO: figure out how to set the text for the class and how to hide/show/resize the transparent rectangle based on class time/length
+        View scheduleBlock1 = rowView.findViewById(R.id.schedule_block1);
+        View scheduleBlock2 = rowView.findViewById(R.id.schedule_block2);
+        //scheduleBlock1.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 0));
+        scheduleBlock2.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 0));
+
+        //sets the box to half the height of the listview item and halfway down (for classes that start at X:30)
+        /*int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 35, context.getResources().getDisplayMetrics());
+        RelativeLayout.LayoutParams schedBlock2Param = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, height);
+        schedBlock2Param.setMargins(0, height, 0, 0);
+        scheduleBlock2.setLayoutParams(schedBlock2Param);*/
+
+
+        TextView courseName = (TextView) rowView.findViewById(R.id.course_name);
+        TextView startEnd = (TextView) rowView.findViewById(R.id.start_end);
+        courseName.setText("");
+        startEnd.setText("");
 
         return rowView;
     }
