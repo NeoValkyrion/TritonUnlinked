0266034dfadd2f5a2b78060310f3003398e8f078
Jeremy Kao <j7kao@ucsd.edu>
2014-03-10 11:48:42 -0700

    commiting browsecoursesadapter; should fix the build

--- TritonUnlinked/src/main/java/triton/unlinked/BrowseCoursesAdapter.java
+++ TritonUnlinked/src/main/java/triton/unlinked/BrowseCoursesAdapter.java
@@ -5,25 +5,30 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class BrowseCoursesAdapter extends BaseAdapter {
     private final Context context;
+    private ArrayList<String> subjList;
     private ArrayList<String> title;
-    private ArrayList<String> description;
+    private ArrayList<String> course;
+    private boolean displayingCourseAndTitle;
 
-    public BrowseCoursesAdapter(Context context, ArrayList<String> title, ArrayList<String> description) {
+    public BrowseCoursesAdapter(Context context, ArrayList<String> course, ArrayList<String> title) {
         this.context = context;
-        this.title = title;
-        this.description = description;
-        Log.i("BrowseCoursesAdapter", "BrowseCoursesAdapter constructor — get title 0th element " + this.title.get(0));
+        setDisplayingCourseAndTitle(false);
+        this.subjList = course;
+        addCourseRow(course, title);
     }
     public int getCount(){
-        return this.title.size();
+        return this.course.size();
     }
     public long getItemId(int position){
         return position;
@@ -31,26 +36,39 @@ public class BrowseCoursesAdapter extends BaseAdapter {
     public Object getItem(int position){
         return position;
     }
-    public void addDescription(ArrayList<String> description){
-        this.description = description;
+    public void setDisplayingCourseAndTitle(boolean val){
+        this.displayingCourseAndTitle = val;
     }
-    public void addTitle(ArrayList<String> title){
+    public boolean getDisplayingCourseAndTitle(){
+        return this.displayingCourseAndTitle;
+    }
+    public void addCourseRow(ArrayList<String> course, ArrayList<String> title){
+        this.course = course;
         this.title = title;
+        if (course.size() > 0 && title.size() > 0 ){
+            setDisplayingCourseAndTitle(true);
+        }
+        else{
+            setDisplayingCourseAndTitle(false);
+        }
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.browse_list_item, parent, false);
+
         TextView titleText = (TextView) rowView.findViewById(R.id.courseTitleListItem);
         TextView descText = (TextView) rowView.findViewById(R.id.courseDescListItem);
-        Log.i("BrowseCoursesAdapter", "BrowseCoursesAdapter.getView() — get title Text " + this.title.get(position));
+
+        if (this.course.size() > position && !this.course.get(position).equalsIgnoreCase(""))
+            titleText.setText(this.course.get(position));
+
         if (this.title.size() > position && !this.title.get(position).equalsIgnoreCase(""))
-            titleText.setText(this.title.get(position));
-        if (this.description.size() > position && !this.description.get(position).equalsIgnoreCase(""))
-            descText.setText(this.description.get(position));
+            descText.setText(this.title.get(position));
         else
             descText.setVisibility(View.GONE);
+
         return rowView;
     }
 }
